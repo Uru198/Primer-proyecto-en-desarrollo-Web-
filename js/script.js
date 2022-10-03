23b55a7ac986284beda35129261c1a993646e93e
Andres Uru <98914498+Uru198@users.noreply.github.com>
2022-10-02 19:43:34 -0500

    inicio

---
+++ js/script.js
@@ -0,0 +1,16 @@
+function carrito(){
+        alert("Your Cart is empy!");
+}
+
+function eliminaCookie (){
+        var elementoCookie = document.querySelector (".cookie");
+        elementoCookie.remove();
+}
+function cambiaruno (suculenta){
+        suculenta.src="css/images/succulents-2.jpg"
+}
+
+
+function cambiardos (suculenta){
+        suculenta.src="css/images/succulents-1.jpg"
+}
